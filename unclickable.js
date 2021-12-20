f16e61bc6ed82c23f0ae03a14d73cb61246d0c14
BasTijs <bastijs@bastijs.nl>
2021-12-20 21:56:41 +0100

    Cleanup
    Cleanup

--- unclickable.js
+++
@@ -1,3 +0,0 @@
-while(true){
-document.getElementById('unclickable').click();
-}
--- weaken.js
+++
@@ -1,7 +0,0 @@
-/**
- * @param {NS} ns
- */
-export async function main(ns) {
-  await ns.sleep(ns.args[0])
-  await ns.weaken(ns.args[1])
-}
